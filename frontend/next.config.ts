45e5fa869d257abf98a9709c0659fa084a8f5b90
prashantnk2001 <prashantnk2001@gmail.com>
2026-03-02 19:04:20 +0000

    add example env, add icon, add supabase proxy

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -4,12 +4,19 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   // This tells Next.js to act as a Proxy for our Backend
   async rewrites() {
+    const supabaseUrl =
+      process.env.NEXT_PUBLIC_SUPABASE_URL?.replace(/\/$/, "") || "";
     return [
       {
         // Any request coming to the frontend starting with /api/
         source: "/api/:path*",
         // Will be secretly forwarded to FastAPI on the local container network!
-        destination: "http://127.0.0.1:8000/api/:path*", 
+        destination: "http://127.0.0.1:8000/api/:path*",
+      },
+      {
+        // Supabase ISP Block Bypass Proxy
+        source: "/supabase-proxy/:path*",
+        destination: `${supabaseUrl}/:path*`,
       },
     ];
   },
